11ef6c15d95108a1c73f00fbf6cea9b44d14d02b
echo <jamesx03@gmail.com>
2020-11-29 21:37:09 -0500

    Updated site with links and full controls

---
+++ markdown-it-config.js
@@ -0,0 +1,6 @@
+var MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();
+var result = md.render('# markdown-it rulezz!');
+
+// $('.md-container').html(md.render($('.md-src').text()));
+
